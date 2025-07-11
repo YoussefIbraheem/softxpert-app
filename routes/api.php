d5cc850d9bdc8ff0c07f3081251ac8cae2e11fdd
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-07-11 19:59:32 +0300

    Laravel Pint Adjustments

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::post('/login', [UserController::class, 'login'])->name('users.login');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout'])->name('users.logout');
     Route::middleware('role:admin')->group(function () {
-    Route::post('/change-role', [UserController::class, 'change_user_role'])->name('users.changeRole');
+        Route::post('/change-role', [UserController::class, 'change_user_role'])->name('users.changeRole');
     });
 });
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -12,87 +12,82 @@ test('admin can change user role to manager', function () {
 
     $this->actingAs($admin);
 
-    $response = $this->post('api/change-role',[
+    $response = $this->post('api/change-role', [
         'user_id' => $user->id,
-        'role_name' => UserRole::MANAGER->value
+        'role_name' => UserRole::MANAGER->value,
     ])->assertStatus(200);
 
     $response->assertJsonFragment([
         'name' => $user->name,
         'email' => $user->email,
-        'role' => UserRole::MANAGER->value
+        'role' => UserRole::MANAGER->value,
     ]);
-
 
 });
 
 test('admin can change manager role to user', function () {
     $admin = User::factory()->create();
-    $manager= User::factory()->create();
+    $manager = User::factory()->create();
 
     $admin->assignRole(UserRole::ADMIN);
     $manager->assignRole(UserRole::MANAGER);
 
     $this->actingAs($admin);
 
-    $response = $this->post('api/change-role',[
+    $response = $this->post('api/change-role', [
         'user_id' => $manager->id,
-        'role_name' => UserRole::USER->value
+        'role_name' => UserRole::USER->value,
     ])->assertStatus(200);
 
     $response->assertJsonFragment([
         'name' => $manager->name,
         'email' => $manager->email,
-        'role' => UserRole::USER->value
+        'role' => UserRole::USER->value,
     ]);
-
 
 });
 
-
-test('admin cannot change other admin role',function(){
+test('admin cannot change other admin role', function () {
 
     $admin_1 = User::factory()->create();
-    $admin_2= User::factory()->create();
-
+    $admin_2 = User::factory()->create();
 
     $admin_1->assignRole(UserRole::ADMIN);
     $admin_2->assignRole(UserRole::ADMIN);
 
     $this->actingAs($admin_1);
 
-    $this->post('api/change-role',[
-        'user_id'=> $admin_1->id,
-        'role_name' => UserRole::USER->value
+    $this->post('api/change-role', [
+        'user_id' => $admin_1->id,
+        'role_name' => UserRole::USER->value,
     ])->assertStatus(403);
 
-    $this->post('api/change-role',[
-        'user_id'=> $admin_1->id,
-        'role_name' => UserRole::MANAGER->value
+    $this->post('api/change-role', [
+        'user_id' => $admin_1->id,
+        'role_name' => UserRole::MANAGER->value,
     ])->assertStatus(403);
 
 });
 
-test('admin cannot change it\'s own role',function(){
+test('admin cannot change it\'s own role', function () {
     $admin = User::factory()->create();
 
     $admin->assignRole(UserRole::ADMIN);
 
     $this->actingAs($admin);
 
-
-    $this->post('api/change-role',[
-        'user_id'=> $admin->id,
-        'role_name'=> UserRole::USER->value
+    $this->post('api/change-role', [
+        'user_id' => $admin->id,
+        'role_name' => UserRole::USER->value,
     ])->assertStatus(403);
 
-    $this->post('api/change-role',[
-        'user_id'=> $admin->id,
-        'role_name'=> UserRole::MANAGER->value
+    $this->post('api/change-role', [
+        'user_id' => $admin->id,
+        'role_name' => UserRole::MANAGER->value,
     ])->assertStatus(403);
 });
 
-test('non-admin cannot change other user\'s role',function(){
+test('non-admin cannot change other user\'s role', function () {
     $user_1 = User::factory()->create();
     $user_2 = User::factory()->create();
 
@@ -101,17 +96,16 @@ test('non-admin cannot change other user\'s role',function(){
 
     $this->actingAs($user_1);
 
-    $this->post('api/change-role',[
-        'user_id'=> $user_2->id,
-        'role_name'=> UserRole::MANAGER->value
+    $this->post('api/change-role', [
+        'user_id' => $user_2->id,
+        'role_name' => UserRole::MANAGER->value,
     ])->assertStatus(403);
 
     $this->actingAs($user_2);
 
-    $this->post('api/change-role',[
-        'user_id'=> $user_1->id,
-        ''=> UserRole::USER->value
+    $this->post('api/change-role', [
+        'user_id' => $user_1->id,
+        '' => UserRole::USER->value,
     ])->assertStatus(403);
 
 });
-
